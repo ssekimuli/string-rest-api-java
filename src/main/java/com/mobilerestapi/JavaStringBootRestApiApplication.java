ca797e49a895d87f04cedb0c87bb9343d785f645
sekimuli <assekimuli@projectcode.ug>
2024-02-28 16:12:28 +0300

    init java rest api

---
+++ src/main/java/com/mobilerestapi/JavaStringBootRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.mobilerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStringBootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaStringBootRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobilerestapi/JavaStringBootRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobilerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStringBootRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
